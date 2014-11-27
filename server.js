43e713f2711d9d1b2e8a1e13eda6ace3f3e8915b
Ron van der Wijngaard <rvdwijngaard@to-increase.com>
2014-11-27 14:23:49 +0100

    commit

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
-//require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 app.get('/', function (req, res) {
     res.send('<b> hello there world</b>');
 });
 
-//app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-//app.get('/auth/google/callback',
-//	    passport.authenticate('google', {
-//        successRedirect : '/profile',
-//        failureRedirect : '/'
-//    }));
+app.get('/auth/google/callback',
+	    passport.authenticate('google', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
 
 //app.get('/profile', isLoggedIn, function (req, res) { 
 //    res.send('<p>hello <b>'+ req.user.get("name") + '</b></p>');
