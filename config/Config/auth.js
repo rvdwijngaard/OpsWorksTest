3c03c9f789aafd9ce0db5d4bd7e5dc97e96ecfaa
Ron van der Wijngaard <rvdwijngaard@to-increase.com>
2014-11-27 18:42:32 +0100

    renamed to config

---
+++ config/Config/auth.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    'googleAuth' : {
+        'clientID'      : 'myid',
+        'clientSecret'  : 'mysecret',
+        'callbackURL'   : 'http://127.0.0.1/auth/google/callback'
+    }
+};
---
+++ config/Config/passport.js
@@ -0,0 +1,42 @@
+﻿var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var configAuth = require('./auth');
+var User = require('../models/account');
+var User = {};
+module.exports = function (passport) {
+    
+    // used to serialize the user for the session
+    passport.serializeUser(function (user, done) {
+        done(null, user.get("email"));
+    });
+    
+    // used to deserialize the user
+    passport.deserializeUser(function (id, done) {
+        User.get(id, function (err, user) {
+            done(err, user);
+        });
+    });    
+
+    passport.use(new GoogleStrategy({
+            clientID        : configAuth.googleAuth.clientID,
+            clientSecret    : configAuth.googleAuth.clientSecret,
+            callbackURL     : configAuth.googleAuth.callbackURL
+        }, 
+        function (token, refreshToken, profile, done) {           
+            var newUser = new User();
+            
+            newUser.set({
+                google_id : profile.id,
+                google_token : token,
+                name  : profile.displayName,
+                email : profile.emails[0].value // pull the first email
+            });
+
+            newUser.save(function (err) {
+                if (err)
+                    throw err;
+                return done(null, newUser);
+            });
+
+        }));
+};
+
